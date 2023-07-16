56107d72e4e25b09ffc64164404692906cca3b0e
Mikita Durunda <n.rob.durunda@gmail.com>
2023-07-16 21:10:25 +0300

    remove debug

--- Assets/Scripts/Controller/CubeRotationController.cs
+++ Assets/Scripts/Controller/CubeRotationController.cs
@@ -38,17 +38,11 @@ public class CubeRotationController : MonoBehaviour {
 
   #region methods
 
-  private void ReadMouseRightDownContext(InputAction.CallbackContext context) {
-    dragging = true;
-    Debug.Log("mouse right down");
-    Debug.Log(dragging);
-  }
+  private void ReadMouseRightDownContext(InputAction.CallbackContext context) => dragging = true;
 
   private void ReadMouseRightUpContext(InputAction.CallbackContext context) {
     timeElapsed = 0;
     dragging = false;
-    Debug.Log("mouse right up");
-    Debug.Log(dragging);
   }
 
   private void RotateTo90Degrees() {
@@ -64,22 +58,21 @@ public class CubeRotationController : MonoBehaviour {
   private Quaternion GetClosestTo90Rotation() {
     var closestRotation = Quaternion.Euler(0, 0, 0);
     var closestAngle = Quaternion.Angle(rotation, closestRotation);
-    for (var x = -180; x <= 180; x += 90)
-    {
-      for (var y = -180; y <= 180; y += 90)
-      {
-        for (var z = -180; z <= 180; z += 90)
-        {
+
+    for (var x = -180; x <= 180; x += 90) {
+      for (var y = -180; y <= 180; y += 90) {
+        for (var z = -180; z <= 180; z += 90) {
           var targetRotation = Quaternion.Euler(x, y, z);
           var angle = Quaternion.Angle(rotation, targetRotation);
-          if (angle < closestAngle)
-          {
+
+          if (angle < closestAngle) {
             closestRotation = targetRotation;
             closestAngle = angle;
           }
         }
       }
     }
+
     return closestRotation;
   }
 
